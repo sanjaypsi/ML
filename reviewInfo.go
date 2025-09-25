688878aa3e14145aa7abbb4dc29527ea4fc3e6aa
Sanjay K. <151900963+sanjaypsi@users.noreply.github.com>
2025-09-25 14:35:55 +0900

    reviewInfo

---
+++ reviewInfo.go
@@ -0,0 +1,362 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/PolygonPictures/central30-web/front/entity"
+	"github.com/PolygonPictures/central30-web/front/repository/model"
+	"gorm.io/gorm"
+)
+
+type ReviewInfo struct {
+	db *gorm.DB
+}
+
+func NewReviewInfo(db *gorm.DB) (*ReviewInfo, error) {
+	info := model.ReviewInfo{}
+
+	// Specification change: https://jira.ppi.co.jp/browse/POTOO-2406
+	migrator := db.Migrator()
+	if migrator.HasTable(&info) && !migrator.HasColumn(&info, "take_path") {
+		if err := migrator.RenameColumn(&info, "path", "take_path"); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := db.AutoMigrate(&info); err != nil {
+		return nil, err
+	}
+
+	return &ReviewInfo{
+		db: db,
+	}, nil
+}
+
+func (r *ReviewInfo) WithContext(ctx context.Context) *gorm.DB {
+	return r.db.WithContext(ctx)
+}
+
+func (r *ReviewInfo) TransactionWithContext(
+	ctx context.Context,
+	fc func(tx *gorm.DB) error,
+	opts ...*sql.TxOptions,
+) error {
+	db := r.WithContext(ctx)
+	return db.Transaction(fc, opts...)
+}
+
+func (r *ReviewInfo) List(
+	db *gorm.DB,
+	params *entity.ListReviewInfoParams,
+) ([]*entity.ReviewInfo, int, error) {
+	stmt := db
+	for i, g := range params.Group {
+		stmt = stmt.Where(fmt.Sprintf("group_%d = ?", i+1), g)
+	}
+	stmt = stmt.Where("`project` = ?", params.Project)
+	if params.Studio != nil {
+		stmt = stmt.Where("`studio` = ?", *params.Studio)
+	}
+	if params.TaskID != nil {
+		stmt = stmt.Where("`task_id` = ?", *params.TaskID)
+	}
+	if params.SubtaskID != nil {
+		stmt = stmt.Where("`subtask_id` = ?", *params.SubtaskID)
+	}
+	if params.Root != nil {
+		stmt = stmt.Where("`root` = ?", *params.Root)
+	}
+	for i, g := range params.Group {
+		stmt = stmt.Where(fmt.Sprintf("`groups`->\"$[%d]\" = ?", i), g)
+	}
+	if params.Relation != nil {
+		stmt = stmt.Where("relation IN (?)", params.Relation)
+	}
+	if params.Phase != nil {
+		stmt = stmt.Where("phase IN (?)", params.Phase)
+	}
+	if params.Component != nil {
+		stmt = stmt.Where("`component` = ?", *params.Component)
+	}
+	if params.Take != nil {
+		stmt = stmt.Where("`take` = ?", *params.Take)
+	}
+
+	order := "`id` desc"
+	if params.OrderBy != nil {
+		order = *params.OrderBy
+	}
+	showDeleted := false
+	if params.ModifiedSince != nil {
+		stmt = stmt.Where("`modified_at_utc` >= ?", *params.ModifiedSince)
+		order = "`modified_at_utc` asc"
+		showDeleted = true
+	} else {
+		stmt.Where("`deleted` = ?", 0)
+	}
+
+	var total int64
+	var m model.ReviewInfo
+	if err := stmt.Model(&m).Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+
+	var models []*model.ReviewInfo
+	perPage := params.GetPerPage()
+	offset := perPage * (params.GetPage() - 1)
+	if err := stmt.Order(
+		order,
+	).Limit(perPage).Offset(offset).Find(&models).Error; err != nil {
+		return nil, 0, err
+	}
+
+	var entities []*entity.ReviewInfo
+	for _, m := range models {
+		entities = append(entities, m.Entity(showDeleted))
+	}
+	return entities, int(total), nil
+}
+
+func (r *ReviewInfo) Get(
+	db *gorm.DB,
+	params *entity.GetReviewParams,
+) (*entity.ReviewInfo, error) {
+	var m model.ReviewInfo
+	if err := db.Where(
+		"`deleted` = ?", 0,
+	).Where(
+		"`project` = ?", params.Project,
+	).Where(
+		"`id` = ?", params.ID,
+	).Take(&m).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, entity.ErrRecordNotFound
+		}
+		return nil, err
+	}
+	return m.Entity(false), nil
+}
+
+func (r *ReviewInfo) Create(
+	tx *gorm.DB,
+	params *entity.CreateReviewInfoParams,
+) (*entity.ReviewInfo, error) {
+	m := model.NewReviewInfo(params)
+	if err := tx.Create(m).Error; err != nil {
+		return nil, err
+	}
+	return m.Entity(false), nil
+}
+
+func (r *ReviewInfo) Update(
+	tx *gorm.DB,
+	params *entity.UpdateReviewInfoParams,
+) (*entity.ReviewInfo, error) {
+	now := time.Now().UTC()
+	modifiedBy := ""
+	if params.ModifiedBy != nil {
+		modifiedBy = *params.ModifiedBy
+	}
+	var m model.ReviewInfo
+	if err := tx.Where(
+		"`deleted` = ?", 0,
+	).Where(
+		"`project` = ?", params.Project,
+	).Where(
+		"`id` = ?", params.ID,
+	).Take(&m).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, entity.ErrRecordNotFound
+		}
+		return nil, err
+	}
+	var modified = false
+	if params.ApprovalStatus != nil {
+		m.ApprovalStatus = *params.ApprovalStatus
+		m.ApprovalStatusUpdatedAtUtc = now
+		modified = true
+	}
+	if params.ApprovalStatusUpdatedUser != nil {
+		m.ApprovalStatusUpdatedUser = *params.ApprovalStatusUpdatedUser
+		m.ApprovalStatusUpdatedAtUtc = now
+		modified = true
+	}
+	if params.WorkStatus != nil {
+		m.WorkStatus = *params.WorkStatus
+		m.WorkStatusUpdatedAtUtc = now
+		modified = true
+	}
+	if params.WorkStatusUpdatedUser != nil {
+		m.WorkStatusUpdatedUser = *params.WorkStatusUpdatedUser
+		m.WorkStatusUpdatedAtUtc = now
+		modified = true
+	}
+	if !modified {
+		return nil, errors.New("no value is given to change")
+	}
+	m.ModifiedAtUTC = now
+	m.ModifiedBy = modifiedBy
+	return m.Entity(false), tx.Save(m).Error
+}
+
+func (r *ReviewInfo) Delete(
+	tx *gorm.DB,
+	params *entity.DeleteReviewInfoParams,
+) error {
+	now := time.Now().UTC()
+	var modifiedBy string
+	if params.ModifiedBy != nil {
+		modifiedBy = *params.ModifiedBy
+	}
+	var m model.ReviewInfo
+	if err := tx.Where(
+		"`deleted` = ?", 0,
+	).Where(
+		"`project` = ?", params.Project,
+	).Where(
+		"`id` = ?", params.ID,
+	).Take(&m).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return entity.ErrRecordNotFound
+		}
+		return err
+	}
+	m.Deleted = m.ID
+	m.ModifiedAtUTC = now
+	m.ModifiedBy = modifiedBy
+	return tx.Save(m).Error
+}
+
+func (r *ReviewInfo) ListAssets(
+	db *gorm.DB,
+	params *entity.AssetListParams,
+) ([]*entity.Asset, int, error) {
+	stmt := db.Model(
+		&ReviewInfo{},
+	).Where(
+		"deleted = ?", 0,
+	).Where(
+		"project = ?", params.Project,
+	).Where(
+		"root = ?", "assets",
+	).Group(
+		"project",
+	).Group(
+		"root",
+	).Group(
+		"group_1",
+	).Group(
+		"relation",
+	)
+
+	var total int64
+	if err := stmt.Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+
+	stmt = stmt.Order(
+		"group_1",
+	).Order(
+		"relation",
+	)
+
+	fmt.Printf("ListAssets A >>>>>>>>>>>>>>>>>>  %s\n", stmt.Statement.SQL.String())
+
+	var reviews []*model.ReviewInfo
+	perPage := params.GetPerPage()
+	offset := perPage * (params.GetPage() - 1)
+	if err := stmt.Select(
+		"project", "root", "group_1", "relation",
+	).Limit(perPage).Offset(offset).Find(&reviews).Error; err != nil {
+		return nil, 0, err
+	}
+	fmt.Printf("ListAssets >>>>>>>>>>>>>>>>>>  %s\n", stmt.Statement.SQL.String())
+	assets := make([]*entity.Asset, len(reviews))
+	for i, review := range reviews {
+		assets[i] = &entity.Asset{
+			Name:     review.Group1,
+			Relation: review.Relation,
+		}
+	}
+
+	return assets, int(total), nil
+}
+
+func (r *ReviewInfo) ListAssetReviewInfos(
+	db *gorm.DB,
+	params *entity.AssetReviewInfoListParams,
+) ([]*entity.ReviewInfo, error) {
+	stmtA := db.Select(
+		"project",
+		"root",
+		"group_1",
+		"relation",
+		"phase",
+		"MAX(modified_at_utc) AS modified_at_utc",
+	).Model(
+		&model.ReviewInfo{},
+	).Where(
+		"project = ?", params.Project,
+	).Where(
+		"root = ?", "assets",
+	).Where(
+		"group_1 = ?", params.Asset,
+	).Where(
+		"relation = ?", params.Relation,
+	).Where(
+		"deleted = ?", 0,
+	).Group(
+		"project",
+	).Group(
+		"root",
+	).Group(
+		"group_1",
+	).Group(
+		"relation",
+	).Group(
+		"phase",
+	)
+
+	stmtB := db.Select(
+		"*",
+	).Model(
+		&model.ReviewInfo{},
+	).Where(
+		"project = ?", params.Project,
+	).Where(
+		"root = ?", "assets",
+	).Where(
+		"group_1 = ?", params.Asset,
+	).Where(
+		"relation = ?", params.Relation,
+	).Where(
+		"deleted = ?", 0,
+	)
+
+	stmt := db.Select(
+		"b.*",
+	).Table(
+		"(?) AS a", stmtA,
+	).Joins(
+		"LEFT OUTER JOIN (?) AS b ON a.project = b.project AND a.root = b.root AND a.group_1 = b.group_1 AND a.relation = b.relation AND a.phase = b.phase AND a.modified_at_utc = b.modified_at_utc", stmtB,
+	)
+
+	//  print stmt for debug
+	fmt.Printf("ListAssetReviewInfos >>>>>>>>>>>>>>>>>>  SQL: %s\n", stmt.Statement.SQL.String())
+	var reviews []*model.ReviewInfo
+	if err := stmt.Scan(&reviews).Error; err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, err
+	}
+
+	fmt.Printf("ListAssetReviewInfos >>>>>>>>>>>>>>>>>>\n", reviews)
+	// Map reviews to entity.ReviewInfo
+	reviewInfos := make([]*entity.ReviewInfo, len(reviews))
+	for i, review := range reviews {
+		reviewInfos[i] = review.Entity(false)
+	}
+	return reviewInfos, nil
+}
